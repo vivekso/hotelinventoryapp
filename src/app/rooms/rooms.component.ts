9ca3208e3c9d0956a3a12f2baf3c6e7312a87928
Vivek <sonkar.vivek@yahoo.com>
2024-01-27 18:25:16 +0530

    initial commit

---
+++ src/app/rooms/rooms.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hinv-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent {
+
+}
