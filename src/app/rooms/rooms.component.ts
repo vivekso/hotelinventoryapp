7e9bab255a33b611d03628b1dae11afc612e9d9d
Vivek <sonkar.vivek@yahoo.com>
2024-01-31 22:13:22 +0530

    learn about the event binding method

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'hinv-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css']
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit{
 
+  constructor() { }
+  ngOnInit():void {
+  }
+
+  numberOfRooms = 10;
+  hiddenRoom = false;
+  toggle() {
+    this.hiddenRoom =!this.hiddenRoom;
+  }
 }
